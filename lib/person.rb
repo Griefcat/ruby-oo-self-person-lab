b9602c54d9f5c5f72863fa70a8ab946f68248514
Griefcat <anardolilli@gmail.com>
2020-10-07 14:54:08 -0400

    our lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,50 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    def initialize(name)
+        @name = name 
+        @bank_account = 25.00
+        @happiness_points = 8
+        @hygiene_points = 8
+        binding.pry
+    end 
+
+
+    def bank_account
+        @bank_account 
+    end 
+
+    def bank_account=(money)
+        @bank_account = money 
+    end 
+
+    attr_accessor :happiness_points 
+    attr_accessor :hygiene_points
+    attr_reader :name
+    attr_writer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end 
+
+bryce = Person.new("Bryce")
+akram = Person.new("Akram")
